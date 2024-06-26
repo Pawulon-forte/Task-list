203958f47a97fabf43d9c9a798cf1006b1ea7973
Paweł Kwit <pawelkwit@Pawes-MacBook-Pro.local>
2024-06-26 21:34:01 +0200

    Add model enums

--- src/app/models/task.ts
+++ src/app/models/task.ts
@@ -3,21 +3,22 @@ export interface Task {
   taskNumber: string;
   name: string;
   description: string;
-  status: string;
-  priority: string;
+  status: Status;
+  priority: Priority;
   percentageOfProgress: number;
   startDate: string;
   endDate: string;
   childTasks: Task[];
 }
 
-// "id": "f90531ca-3e23-4c1c-9b76-a5aee6496791",
-// "taskNumber": "5-003-1",
-// "name": "Design Website Mockup",
-// "description": "Create an initial design mockup for the client's new website.",
-// "status": "TO_DO",
-// "priority": "HIGH",
-// "percentageOfProgress": 85,
-// "startDate": "2024-06-01",
-// "endDate": "2024-06-07",
-// "childTasks": [
+export enum Status {
+  ToDO = 'TODO',
+  InProgress = 'IN_PROGRESS',
+  Done = 'DONE',
+}
+
+export enum Priority {
+  Normal = 'NORMAL',
+  Medium = 'MEDIUM',
+  Hight = 'HIGH',
+}
